843e6e1bdf3f2a87f672cab89c0b20d9b75d58cc
VolkenK <volkenkoelewijn@gmail.com>
2019-09-02 14:36:29 +0200

    spawner gamaakt
    ik heb een spawner script gemaakt voor de boulder opjecten

---
+++ 80's Game Jam/Assets/Boulder_spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boulder_spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject BoulderPrefab;
+    [SerializeField]  private float spawnRate =30;
+    private float timeLeft =0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //elke 2 seconden kiezen of er een gespawned word, en dan elke keer kiezen of er dubbel gespawned word.
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            timeLeft = 60 / spawnRate * Random.Range(0.75f,1.25f);
+            SpawnBoulder();
+            Debug.Log("sfgsdgglslgkgjk");
+        }
+    }
+
+    private void SpawnBoulder()
+    {
+        Instantiate(BoulderPrefab, transform);
+    }
+}
